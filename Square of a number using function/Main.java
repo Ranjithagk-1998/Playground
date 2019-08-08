2919338d9ca428d61b4966e7347747cbcf25b8eb
Ranjithagk-1998 <50174520+Ranjithagk-1998@users.noreply.github.com>
2019-08-08 20:56:21 +0530

    Test code pushed to repository.

---
+++ Square of a number using function/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main
+{
+	public static void main (String[] args)
+    {
+      Scanner in = new Scanner(System.in);
+	 int n = in.nextInt();
+      System.out.print(square_of_a_numbers(n));
+    }
+  public static int square_of_a_numbers(int a)
+  {
+    int result = a * a;
+    return result;
+  
+
+	} 
+}
